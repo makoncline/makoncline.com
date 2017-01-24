52cb31060c26724cd9c52509f9be3e9cecd64dd0
makoncline <ogmakon@gmail.com>
2017-01-24 13:13:36 -0600

    Update mightygumball.js

--- MightyGumball/mightygumball.js
+++ MightyGumball/mightygumball.js
@@ -1,3 +1,6 @@
+
+var lastReportTime = 0;
+
 window.onload = function (){
 	
 }
@@ -12,10 +15,14 @@ function updateSales(sales){
 		div.innerHTML = sale.name + " sold" + sale.sales + " gumballs";
 		salesDiv.appendChild(div);
 	}
+	if (sales.length>0) {
+		lastReportTime = sales[sales.length-1].time;
+	}
+	
 }
 
 function handleRefresh(){
-	var url = "http://gumball.wickedlysmart.com/?callback=updateSales" + "&random=" + (newDate()).getTime();
+	var url = "http://gumball.wickedlysmart.com" +"?callback=updateSales" + "&lastReportTime=" + lastReportTime + "&random=" + (newDate()).getTime();
 	var newScriptElement = document.createElement("script");
 	newScriptElement.setAttribute("src", url);
 	newScriptElement.setAttribute("id", "jsonp";
